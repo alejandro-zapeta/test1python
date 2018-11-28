0a59e11808178d4b634d8f26a0525c1e047fcf0f
jazapetap <jazapetap@via-asesores.com>
2018-11-28 15:58:37 -0600

    token1

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from manage_api import generate_token
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello XXXX!"
+	
+@app.route("/rs/generate-token")
+def rs_generate_token():
+    return generate_token()
