41aa14f572f32bc4eca552a9959c92c2c7f1211c
jazapetap <jazapetap@via-asesores.com>
2018-11-28 17:31:37 -0600

    tok2

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ def install_and_load(package):
 	import subprocess
 	subprocess.call(['pip', 'install', package])
 	f, fname, desc = imp.find_module(package)
-	return imp.load(package, f, fname, desc)
+	return imp.load_module(package, f, fname, desc)
 	
 try:
 	import requests;
