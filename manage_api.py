c55648f00cb2e3291cb7bc8807255597053490c2
jazapetap <jazapetap@via-asesores.com>
2018-11-28 17:46:55 -0600

    tok2

--- manage_api.py
+++ manage_api.py
@@ -13,15 +13,17 @@ CLIENT_ID = "afbda1af-5662-4364-9705-d3b5bf9e0bc9";
 APPLICATION_DOMAIN = "GruposegaGT.onmicrosoft.com";
 
 
-
 def generate_token():
-    login_url = ("%s/%s/oauth2/token"  % (AUTHORITY, APPLICATION_DOMAIN));
-    data_to_post = {"resource": RESOURCE_URL, "client_id": CLIENT_ID,
-                    "grant_type": "password", "username": USERNAME, "password": PASSWORD,
-                    "scope": "openid"}
+	try:
+		login_url = ("%s/%s/oauth2/token"  % (AUTHORITY, APPLICATION_DOMAIN));
+		data_to_post = {"resource": RESOURCE_URL, "client_id": CLIENT_ID,
+						"grant_type": "password", "username": USERNAME, "password": PASSWORD,
+						"scope": "openid"}
 
-    r = requests.post(login_url, data = data_to_post)
-    return r.json();
+		r = requests.post(login_url, data = data_to_post)
+		return r.json();
+	except Exception as e:
+		return str(e)
 
 #print(generate_token());
 
