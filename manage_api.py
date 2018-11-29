52c1870a654cf4615cf3d1933dbd147eb191fdba
jazapetap <jazapetap@via-asesores.com>
2018-11-29 09:45:46 -0600

    tok2

--- manage_api.py
+++ manage_api.py
@@ -1,10 +1,8 @@
 from mimain import install_and_load
 try:
-	import requests;
+    import requests;
 except:
-	requests = install_and_load('requests')
-	if requests == None:
-		return "ERROR 10001";
+    requests = install_and_load('requests')
 
 PARTNER_SERVICE_APIROOT = "https://api.partnercenter.microsoft.com";
 AUTHORITY = "https://login.windows.net";
@@ -14,20 +12,21 @@ RESOURCE_URL = "https://api.partnercenter.microsoft.com";
 CLIENT_ID = "afbda1af-5662-4364-9705-d3b5bf9e0bc9";
 APPLICATION_DOMAIN = "GruposegaGT.onmicrosoft.com";
 
-
 def generate_token():
-	try:
-		login_url = ("%s/%s/oauth2/token"  % (AUTHORITY, APPLICATION_DOMAIN));
-		data_to_post = {"resource": RESOURCE_URL, "client_id": CLIENT_ID,
-						"grant_type": "password", "username": USERNAME, "password": PASSWORD,
-						"scope": "openid"}
+    if requests == None:
+        return "ERROR not loading requests";
+    try:
+        login_url = ("%s/%s/oauth2/token" % (AUTHORITY, APPLICATION_DOMAIN));
+        data_to_post = {"resource": RESOURCE_URL, "client_id": CLIENT_ID,
+                        "grant_type": "password", "username": USERNAME, "password": PASSWORD,
+                        "scope": "openid"}
 
-		r = requests.post(login_url, data = data_to_post)
-		return r.json();
-	except Exception as e:
-		return str(e)
+        r = requests.post(login_url, data=data_to_post)
+        return r.json();
+    except Exception as e:
+        return str(e)
 
-#print(generate_token());
+# print(generate_token());
 
-#def prueba():
+# def prueba():
 #	return "123";
